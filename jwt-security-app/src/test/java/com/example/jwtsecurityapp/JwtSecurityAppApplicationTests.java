d4e36687bf56415ccbc4b370b9761b4597584976
andrei <ochian_a@yahoo.ro>
2023-12-13 08:05:47 +0200

    feat: Reinitialize project

---
+++ jwt-security-app/src/test/java/com/example/jwtsecurityapp/JwtSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
