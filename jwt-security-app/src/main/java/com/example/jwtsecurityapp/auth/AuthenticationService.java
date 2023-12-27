91b824539017804778823d358e84b92260c8c4f9
andrei <ochian_a@yahoo.ro>
2023-12-27 18:28:23 +0200

    feat: Implement authentication and register logic in AuthenticationService

--- jwt-security-app/src/main/java/com/example/jwtsecurityapp/auth/AuthenticationService.java
+++ jwt-security-app/src/main/java/com/example/jwtsecurityapp/auth/AuthenticationService.java
@@ -1,7 +1,13 @@
 package com.example.jwtsecurityapp.auth;
 
+import com.example.jwtsecurityapp.User.Role;
+import com.example.jwtsecurityapp.User.User;
 import com.example.jwtsecurityapp.User.UserRepository;
+import com.example.jwtsecurityapp.config.JWTService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,13 +15,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
 
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
-        return  null;
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))//se si cripteaza parola
+                .role(Role.USER)
+                .build();
+        repository.save(user);//salvam userul in baza de date
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken).build();
     }
 
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(/**Metoda are grija sa mi returneze inclusiv erori daca email ul sau parola nu sunt corecte*/
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        //totul a fost ok si general un token pe care sa l trimitem inapoi
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken).build();
     }
 }
